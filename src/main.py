eb2c22c78fb92878b231b6d7995e8ca1b4fc552e
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-05 00:36:30 -0800

    recognizes .mid file from data folder

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 import mido
+from pathlib import Path
+
+midi_file = None
+midi_file_path = Path(__file__).parent.parent / 'data'
+for n in midi_file_path.iterdir():
+    if n.suffix.lower() == '.mid':
+        midi_file = n
+        break
+
+if midi_file is None:
+    midi_file = 'cannot find midi file'
+print(midi_file)

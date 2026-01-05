cda0a10b03265c5ea73ad6252fb49921cdc53f2e
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-05 19:43:45 +0000

    awdhw

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
-import mido
+from mido import MidiFile
 from pathlib import Path
 
-#Access midifile from data folder
 midi_file = None
 midi_file_path = Path(__file__).parent.parent / 'data'
+
+#Access midifile from data folder
 for n in midi_file_path.iterdir():
     if n.suffix.lower() == '.mid':
-        midi_file = n
+        midi_file = MidiFile(n)
         break
 
 if midi_file is None:
     midi_file = 'cannot find midi file'
-print(midi_file)
+log = open(midi_file_path)
---
+++ src/test.py
@@ -0,0 +1,5 @@
+from mido import MidiFile
+from pathlib import Path
+
+mid = MidiFile('/workspaces/MIDIParser/data/swan-lake-no.-10-scne-in-b-minor-opus-20--tchaikovsky.mid')
+print(mid)

a983abf8c28730d841310820bbe7b1d18b5295b0
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-06 19:43:29 +0000

    logs

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 from mido import MidiFile
 from pathlib import Path
+from datetime import datetime
 
 midi_file = None
 midi_file_path = Path(__file__).parent.parent / 'data'
+log_path = Path(__file__).parent.parent / 'output'
 
 #Access midifile from data folder
 for n in midi_file_path.iterdir():
     if n.suffix.lower() == '.mid':
         midi_file = MidiFile(n)
         break
-
 if midi_file is None:
     midi_file = 'cannot find midi file'
-log = open(midi_file_path)
+
+#create log file with timestamp
+log = open(log_path / f'log_{datetime.now().strftime("%Y%m%d_%H%M%S")}.txt', 'w')
+log.write(str(midi_file))
+log.close()
--- src/test.py
+++ src/test.py
@@ -2,4 +2,5 @@ from mido import MidiFile
 from pathlib import Path
 
 mid = MidiFile('/workspaces/MIDIParser/data/swan-lake-no.-10-scne-in-b-minor-opus-20--tchaikovsky.mid')
-print(mid)
+log = open('mid.txt', 'w')
+log.write(str(mid))

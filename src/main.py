9540d897c6b0ab862505cdd4e786586be524c92f
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-29 04:50:03 +0000

    json structure basic format

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from mido import MidiFile
 from pathlib import Path
 from datetime import datetime
+import json
 
 midi_file = None
 midi_file_path = Path(__file__).parent.parent / 'data'
+
+json_file = []
 
 #Access midifile from data folder
 for n in midi_file_path.iterdir():
@@ -13,9 +16,24 @@ for n in midi_file_path.iterdir():
 if midi_file is None:
     midi_file = 'cannot find midi file'
 
-#change to any variable to write on log
-logwrite = str(midi_file)
+#Form output json
+for tracks in midi_file.tracks:
+    track_data = {}
+    time_signatures = []
+    key_signatures = []
+    for msg in tracks:
+        if msg.type == 'track_name':
+            track_data['instrument_name'] = msg.name
+        if msg.type == 'time_signature':
+            time_signatures.append({'numerator': msg.numerator, 'denominator': msg.denominator})
+            track_data['time_signature'] = time_signatures
+        if msg.type == 'key_signature':
+            key_signatures.append({'key': msg.key})
+            track_data['key_signature'] = key_signatures
+    json_file.append(track_data)
 
+#change to any variable to write on log
+logwrite = json.dumps(json_file, indent=2)
 
 #create log file with timestamp
 log_path = Path(__file__).parent.parent / 'output'
